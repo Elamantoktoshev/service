137757eeec76083f23e847761f074ee0aea5d054
Elaman Toktoshev <elamantoktoshev02@gmail.com>
2022-11-17 05:26:21 +0600

    Finish project

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SEC_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mytest',
+        'USER': 'elaman',
+        'PASSWORD': 'qwer',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
